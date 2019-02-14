b17e588542f82de4b3065654599e994c091d20e6
yashaswi maharshi <maha.yash@gmail.com>
2019-02-14 22:37:12 +0530

    UI for Product list

--- src/components/ProductListItem/index.tsx
+++ src/components/ProductListItem/index.tsx
@@ -1,7 +1,10 @@
-import "./scss/index.scss";
-
+//import "./scss/index.scss";
+import "../../vendor/bootstrap/css/bootstrap.css";
+import "../../css/util.min.css";
+import "../../css/main.min.css";
+import { generateProductUrl } from "../../core/utils";
 import * as React from "react";
-
+import { Link } from "react-router-dom";
 import { CachedThumbnail } from "..";
 import { BasicProductFields } from "../../views/Product/types/BasicProductFields";
 
@@ -24,15 +27,23 @@ interface ProductListItemProps {
 const ProductListItem: React.FC<ProductListItemProps> = ({ product }) => {
   const { price, category } = product;
   return (
-    <div className="product-list-item">
-      <div className="product-list-item__image">
-        <CachedThumbnail source={product}>
-          <img src={noPhotoImg} alt={product.thumbnail.alt} />
-        </CachedThumbnail>
+    <div className="col-sm-12 col-md-6 col-lg-3 p-b-50">
+      <div className="block2">
+        <div className="block2-img wrap-pic-w of-hidden pos-relative block2-labelnew">
+          <CachedThumbnail source={product}>
+            <img src={noPhotoImg} alt={product.thumbnail.alt} />
+          </CachedThumbnail>
+        </div>
+        <div className="block2-txt p-t-20" style={{ textAlign: "center" }}>
+          <Link
+            className="block2-name dis-block s-text3 p-b-5"
+            to={generateProductUrl(product.id, product.name)}
+          >
+            {product.name} <br />
+            {price.localized}
+          </Link>
+        </div>
       </div>
-      <h4 className="product-list-item__title">{product.name}</h4>
-      <p className="product-list-item__category">{category.name}</p>
-      <p className="product-list-item__price">{price.localized}</p>
     </div>
   );
 };
--- src/components/ProductsList/index.tsx
+++ src/components/ProductsList/index.tsx
@@ -1,10 +1,13 @@
-import "./scss/index.scss";
+//import "./scss/index.scss";
+import "../../vendor/bootstrap/css/bootstrap.css";
+import "../../css/util.min.css";
+import "../../css/main.min.css";
 
 import * as React from "react";
 import { Link } from "react-router-dom";
 
 import { Button, Dropdown, ProductListItem } from "..";
-import { generateProductUrl } from "../../core/utils";
+
 import Loader from "../Loader";
 import { Filters } from "../ProductFilters";
 
@@ -43,60 +46,64 @@ export const ProductList: React.FC<ProductsListProps> = ({
   const hasProducts = !!totalCount;
 
   return (
-    <div className="products-list">
-      <div className="products-list__products container">
-        <div className="products-list__products__subheader">
-          {hasProducts && (
-            <span className="products-list__products__subheader__total">
-              {totalCount} Products
-            </span>
-          )}
-          {displayLoader && (
-            <div className="products-list__loader">
-              <Loader />
-            </div>
-          )}
-          <span className="products-list__products__subheader__sort">
-            {hasProducts && (
-              <>
-                <span>Sort by:</span>{" "}
-                <Dropdown
-                  options={filterOptions}
-                  value={sortValues || ""}
-                  isSearchable={false}
-                  onChange={event => onOrder(event.value)}
-                />
-              </>
-            )}
-          </span>
-        </div>
-        {hasProducts ? (
-          <>
-            <div className="products-list__products__grid">
-              {products.map(product => (
-                <Link
-                  to={generateProductUrl(product.id, product.name)}
-                  key={product.id}
-                >
-                  <ProductListItem product={product} />
-                </Link>
-              ))}
-            </div>
-            <div className="products-list__products__load-more">
-              {displayLoader ? (
-                <Loader />
-              ) : (
-                hasNextPage && (
-                  <Button secondary onClick={onLoadMore}>
-                    Load more products
-                  </Button>
-                )
+    <div className="bgwhite p-t-55 p-b-65">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 col-md-12 col-lg-12 p-b-50">
+            <div className="flex-sb-m flex-w p-b-35">
+              {hasProducts && (
+                <span className="s-text8 p-t-5 p-b-5">
+                  {totalCount} Products
+                </span>
               )}
+              {displayLoader && (
+                <div className="products-list__loader">
+                  <Loader />
+                </div>
+              )}
+              <span className="flex-w">
+                <div className="rs2-select2 bo4 of-hidden w-size12 m-t-5 m-b-5 m-r-10">
+                  {hasProducts && (
+                    <>
+                      {/* <span>Sort by:</span>{" "} */}
+                      <Dropdown
+                        className="selection-2 select2-hidden-accessible"
+                        options={filterOptions}
+                        value={sortValues || ""}
+                        isSearchable={false}
+                        onChange={event => onOrder(event.value)}
+                      />
+                    </>
+                  )}
+                </div>
+              </span>
             </div>
-          </>
-        ) : (
-          <div className="products-list__products-not-found">{notFound}</div>
-        )}
+            {hasProducts ? (
+              <>
+                <div className="row">
+                  {products.map(product => (
+                    <ProductListItem product={product} key={product.id} />
+                  ))}
+                </div>
+                <div className="products-list__products__load-more">
+                  {displayLoader ? (
+                    <Loader />
+                  ) : (
+                    hasNextPage && (
+                      <Button secondary onClick={onLoadMore}>
+                        Load more products
+                      </Button>
+                    )
+                  )}
+                </div>
+              </>
+            ) : (
+              <div className="products-list__products-not-found">
+                {notFound}
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
